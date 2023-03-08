079c4b59ccf2f5e3333f0dc2af918eb32b11af3f
Raul Dias <raul.dias@avios.com>
2023-03-07 22:39:16 -0300

    provavelmente vai funcionar.

--- src/main/java/br/com/queensburger/queensBurger/model/OrderProduct.java
+++ src/main/java/br/com/queensburger/queensBurger/model/OrderProduct.java
@@ -1,32 +1,57 @@
 package br.com.queensburger.queensBurger.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name = "orderProduct")
+@Table(name = "orderproducts")
 public class OrderProduct {
-  
-  @ManyToOne
-  @JoinTable(name = "orderProduct",
-    joinColumns = {@JoinColumn(name = "orderId")}, 
-    inverseJoinColumns = {@JoinColumn(name = "id")} 
-  )
-  private Order orderId;
-  
-  @ManyToOne
-  @JoinTable(name = "orderProduct",
-    joinColumns = {@JoinColumn(name = "productId")}, 
-    inverseJoinColumns = {@JoinColumn(name = "id")} 
-  )
-  private Product product;
 
-  @Column
-  private Integer qtd;
+    @EmbeddedId
+    private OrderProductsKey id; // <-- Essa é a chave composta.
 
+    @ManyToOne
+    @MapsId("orderId") // <-- Esse aqui mapea o atributo da chave composta
+    @JoinColumn(name = "orderid") // <-- Esse aqui mapea o nome da coluna no banco
+    private Order order;
+
+    @ManyToOne
+    @MapsId("productId") // <-- Esse aqui mapea o atributo da chave composta
+    @JoinColumn(name = "productid") // <--  Esse aqui mapea o nome da coluna no banco
+    private Product product;
+
+    @Column
+    private Integer qtd;
+
+    public OrderProductsKey getId() {
+        return id;
+    }
+
+    public void setId(OrderProductsKey id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Integer qtd) {
+        this.qtd = qtd;
+    }
 }
 
